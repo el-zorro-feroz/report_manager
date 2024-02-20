3e48f42006beb12b1a657ca1db223721ff74c2ff
el-zorro-feroz <90259229+el-zorro-feroz@users.noreply.github.com>
2024-02-20 05:20:25 +0300

    feat: define global injector

---
+++ lib/core/injector/injector.dart
@@ -0,0 +1,9 @@
+import 'dart:async';
+
+import 'package:get_it/get_it.dart';
+
+/// Global application services locator.
+final GetIt injector = GetIt.I;
+
+/// Initilize [injector] data.
+FutureOr<void> initInjector() async {}
