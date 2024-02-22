a1f4d5c445fa4927fcd0568e8478639724aa598e
el-zorro-feroz <pocket.red.fox@gmail.com>
2024-02-22 16:57:15 +0300

    feat: provide gsheets service to home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,41 @@
-void main() {}
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:gsheets/gsheets.dart';
+import 'package:report_manager/core/api/google_api.dart';
+import 'package:report_manager/core/injector/injector.dart';
+import 'package:report_manager/src/presentation/app.dart';
+import 'package:report_manager/src/services/gsheets_service.dart';
+
+/// HARD_CODED_NUMBER
+const fuelPerHour = 6.9;
+
+Future<void> test() async {
+  final gSheets = GSheets(gSheetsAccountCredentials);
+  final ss = await gSheets.spreadsheet(gSheetsSSID);
+  final ws = ss.worksheetByTitle('report');
+  if (ws == null) return;
+
+  final date = '02.02.2024';
+  final publisher = 2;
+  final special = 2;
+  final vAnte = '=\$E${ws.rowCount}';
+  final vPost = 1111.1;
+  final refuel = 0;
+  final spent = 10.1;
+  final result = '=\$H${ws.rowCount}-\$G${ws.rowCount + 1}+\$F${ws.rowCount + 1}';
+
+  await ws.values.insertRow(
+    ws.rowCount + 1,
+    [date, publisher, special, vAnte, vPost, refuel, spent, result],
+  );
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initInjector();
+  await injector<GSheetsService>().connect();
+
+  runApp(const App());
+}
--- lib/src/presentation/pages/home_page.dart
+++ lib/src/presentation/pages/home_page.dart
@@ -1,11 +1,15 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:gsheets/gsheets.dart';
+import 'package:report_manager/core/failure/failure.dart';
+import 'package:report_manager/core/injector/injector.dart';
 import 'package:report_manager/src/data/report_entry.dart';
 import 'package:report_manager/src/presentation/dialogs/new_report_dialog.dart';
 import 'package:report_manager/src/presentation/widgets/control_panel_widget.dart';
 import 'package:report_manager/src/presentation/widgets/report_entry_widget.dart';
 import 'package:report_manager/src/presentation/widgets/table_description_widget.dart';
+import 'package:report_manager/src/services/gsheets_service.dart';
 
 /// Reporting display page for spec time period and dashboard.
 /// Displayed after the database has been accessed, otherwise, an error screen.
@@ -13,22 +17,97 @@ class HomePage extends StatelessWidget {
   /// Returns an instance of [HomePage].
   const HomePage({super.key});
 
-  ///
-  void openNewReportDialog(BuildContext context) {
-    unawaited(showGeneralDialog(context: context, pageBuilder: (_, __, ___) => const NewReportDialog()));
+  @override
+  Widget build(BuildContext context) {
+    final gsService = injector<GSheetsService>();
+
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(6),
+        child: ListenableBuilder(
+          listenable: gsService,
+          builder: (_, __) {
+            return gsService.state.when(
+              init: _InitHomePage.new,
+              loaded: _LoadedHomePage.new,
+              error: _ErrorHomePage.new,
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class _InitHomePage extends StatelessWidget {
+  const _InitHomePage();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: CircularProgressIndicator(
+        strokeWidth: 1,
+      ),
+    );
+  }
+}
+
+class _LoadedHomePage extends StatelessWidget {
+  const _LoadedHomePage(this.sheets, this.spreadsheet);
+
+  final GSheets sheets;
+  final Spreadsheet spreadsheet;
+
+  Future<List<List<String>>> getData(Worksheet worksheet) async {
+    return worksheet.values.allRows(fromRow: 2);
+  }
+
+  void _openNewReportDialog(BuildContext context) {
+    unawaited(
+      showGeneralDialog(
+        context: context,
+        pageBuilder: (_, __, ___) => const NewReportDialog(),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(6),
-        child: CustomScrollView(
+    final worksheet = spreadsheet.worksheetByTitle('report');
+
+    if (worksheet == null) {
+      return const Center(
+        child: Text('Документ не найден'),
+      );
+    }
+
+    return FutureBuilder(
+      future: getData(worksheet),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return const Center(
+            child: CircularProgressIndicator(
+              strokeWidth: 1,
+            ),
+          );
+        }
+
+        if (snapshot.hasError) {
+          return const Center(
+            child: Text(
+              'Невозможно получить данные с сервера',
+            ),
+          );
+        }
+
+        final data = snapshot.data!;
+
+        return CustomScrollView(
           slivers: [
             SliverToBoxAdapter(
               child: ControlPanelWidget(
                 onNewReportPressed: () {
-                  openNewReportDialog(context);
+                  _openNewReportDialog(context);
                 },
               ),
             ),
@@ -36,8 +115,9 @@ class HomePage extends StatelessWidget {
               child: TableDescriptionWidget(),
             ),
             SliverList.builder(
-              itemCount: 40,
-              itemBuilder: (_, __) {
+              itemCount: data.length,
+              itemBuilder: (_, index) {
+                print(data.elementAt(index).elementAt(1));
                 final reportEntry = ReportEntry(
                   date: DateTime.now(),
                   publisher: 'Савин',
@@ -55,7 +135,22 @@ class HomePage extends StatelessWidget {
               },
             ),
           ],
-        ),
+        );
+      },
+    );
+  }
+}
+
+class _ErrorHomePage extends StatelessWidget {
+  const _ErrorHomePage(this.failure);
+
+  final Failure failure;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: CircularProgressIndicator(
+        strokeWidth: 1,
       ),
     );
   }
