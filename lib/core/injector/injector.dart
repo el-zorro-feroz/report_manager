7169a3d198713e0dceee93e923fa19b975a88c39
el-zorro-feroz <pocket.red.fox@gmail.com>
2024-02-22 16:07:18 +0300

    feat: inject sheets service

--- lib/core/injector/injector.dart
+++ lib/core/injector/injector.dart
@@ -1,9 +1,12 @@
 import 'dart:async';
 
 import 'package:get_it/get_it.dart';
+import 'package:report_manager/src/services/gsheets_service.dart';
 
 /// Global application services locator.
 final GetIt injector = GetIt.I;
 
 /// Initilize [injector] data.
-FutureOr<void> initInjector() async {}
+FutureOr<void> initInjector() async {
+  injector.registerLazySingleton(GSheetsService.new);
+}
